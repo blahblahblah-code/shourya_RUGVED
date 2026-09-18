4f5e62588d73a1e4bc408ef96b671f32174598ae
blahblahblah-code <shouryasharma2551@gmail.com>
2026-09-18 17:18:19 +0530

    Delete 06.py

--- 06.py
+++
@@ -1,8 +0,0 @@
-def anag(str1,str2):
-    return sorted(str1.lower())==sorted(str2.lower())
-str1=input("Enter first string:")
-str2=input("Enter second string:")
-if anag(str1,str2)==True:
-    print("Anagram")
-else:
-    print("Not Anagram")
